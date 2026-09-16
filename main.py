33271f2f408c73da2735ce3fb261cc08a461a556
sinofseven <em.s.00001@gmail.com>
2026-09-16 18:18:44 +0900

    tmp 2026-09-16 18:18:44.168847000+09:00

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ class ResponseFormat(BaseModel):
     nums: list[int]
 
 
-client = OpenAI(
-    base_url="http://localhost:11434",
-    api_key="dummy",  # ollamaでは必要ないけど必須なので
-)
+client = OpenAI(base_url="http://localhost:11434/v1", api_key="dummy")
 
 resp = client.responses.parse(
     model=os.environ["MODEL_NAME"],
+    input=[
+        {
+            "role": "user",
+            "content": "Please calculate the first 30 numbers of the Fibonacci sequence and output them as an array.",
+        }
+    ],
     text_format=ResponseFormat,
-    input="Please calculate the first 30 numbers of the Fibonacci sequence and output them as an array.",
 )
 
+## Debugのために全部出力する
 print(resp.model_dump_json(indent=2, ensure_ascii=False))
